a329da0eee60a4f212afa7b53c5cba665c4eac07
Jarrett Allen Young <5515581+JarrettAllenYoung@users.noreply.github.com>
2024-12-21 18:54:54 -0800

    Initial Commit

---
+++ BounceGPS/BounceGPSApp.swift
@@ -0,0 +1,17 @@
+//
+//  BounceGPSApp.swift
+//  BounceGPS
+//
+//  Created by Jarrett Young on 12/21/24.
+//
+
+import SwiftUI
+
+@main
+struct BounceGPSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BounceGPSTests/BounceGPSTests.swift
@@ -0,0 +1,17 @@
+//
+//  BounceGPSTests.swift
+//  BounceGPSTests
+//
+//  Created by Jarrett Young on 12/21/24.
+//
+
+import Testing
+@testable import BounceGPS
+
+struct BounceGPSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
